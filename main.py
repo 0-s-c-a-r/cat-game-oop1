9b38271d13960c387885df23d16f67a8a04fe7df
0-s-c-a-r <oscar.mariyanayagam@laetottenham.ac.uk>
2024-11-05 15:16:41 +0000

    nice

---
+++ main.py
@@ -0,0 +1,13 @@
+from cat import Cat
+print("Welcome to my cat game")
+naem =  input("whats the name of the cat")
+colour = input("enter a cat colour")
+while True:
+    action = input("""
+What would you like to do?
+1.Train
+2.Feed
+3.Play
+4.Sleep
+""")
+    
